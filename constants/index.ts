69f732b2e1d74e65976691a535e3e306ddea8b04
Peter Ferguson <peterferguson95@gmail.com>
2025-07-02 21:43:36 +0100

    fix: proxy requests
    there is an issue with bun & miniflare crashing atm

--- constants/index.ts
+++ constants/index.ts
@@ -1,12 +1,12 @@
-import z from "zod";
+import z from 'zod';
 
 /* Environment variables schema */
 const envSchema = z.object({
-    WALLET_KEY: z.string(),
-    ENCRYPTION_KEY: z.string(),
-    XMTP_ENV: z.enum(['dev', 'production']),
-    ONIT_API_KEY: z.string(),
-    ONIT_API_URL: z.string().optional(),
+	WALLET_KEY: z.string(),
+	ENCRYPTION_KEY: z.string(),
+	XMTP_ENV: z.enum(['dev', 'production']),
+	ONIT_API_KEY: z.string(),
+	ONIT_API_URL: z.string().optional(),
 });
 
 /* Parse and export environment variables */
@@ -14,6 +14,7 @@ export const ENV = envSchema.parse(process.env);
 
 /* Derived constants */
 export const API_URL = process.env.NODE_ENV === 'production' ? ENV.ONIT_API_URL : 'http://localhost:8787';
+export const PROXY_URL = 'http://localhost:3000/proxy';
 
 /* XMTP specific constants */
 export const WALLET_KEY = ENV.WALLET_KEY;
@@ -24,4 +25,8 @@ export const XMTP_ENV = ENV.XMTP_ENV;
 export const ONIT_API_KEY = ENV.ONIT_API_KEY;
 
 export const ONIT_TEST_TRIGGERS = ['@onit-test', '@onit-test.base.eth'];
-export const ONIT_TRIGGERS = ["@onit", "@onit.base.eth", ...(process.env.NODE_ENV !== 'production' ? ONIT_TEST_TRIGGERS : [])];
+export const ONIT_TRIGGERS = [
+	'@onit',
+	'@onit.base.eth',
+	...(process.env.NODE_ENV !== 'production' ? ONIT_TEST_TRIGGERS : []),
+];
